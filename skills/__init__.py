80d730d810c0263a93cb9c03af1763e5c9fc0ecd
Eddy <832737+eddygk@users.noreply.github.com>
2025-06-05 08:57:47 -0400

    Skills modules: __init__ and github_agent

---
+++ skills/__init__.py
@@ -0,0 +1,10 @@
+"""
+ByteCore Skills - Modular capabilities for the agent
+
+This package contains individual skill modules that extend
+ByteCore's functionality. Each skill is self-contained and
+can be dynamically loaded by the SkillLoader.
+"""
+
+# Skills will be dynamically loaded, no need to import them here
+__all__ = []
